0d11609dabb28f36d5f88c86780d4d3b6591b2bd
Sleep2122 <volodya-stepanov-68@mail.ru>
2023-01-01 20:26:51 +0300

    rw1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Stack L =new Stack();
+
+public class Stack
+{
+    	
+    public List<string> list = new List<string>();
+    public void Add(string bukv)
+    {
+        list.Add(bukv);
+    }
+    public void Pop()
+    {
+        try{
+            list.RemoveAt(list.Count-1);
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("Стек пустой");
+        }
+    }
+    public void Size()
+    {
+        Console.WriteLine(list.Count);
+    }
+    public void Top()
+    {
+        try
+        {
+        Console.WriteLine(list[list.Count-1]);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("0");
+        }
+    }   
+    
+}
